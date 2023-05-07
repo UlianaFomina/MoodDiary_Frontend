5ea2c239951558402c1f98084f595b8ae1e22893
UlianaFomina <6758599@gmail.com>
2023-05-07 20:53:50 +0300

    finish tamplate for newEntry page

--- src/pages/newEntry/newEntry.js
+++ src/pages/newEntry/newEntry.js
@@ -7,9 +7,10 @@ export const NewEntry = ({...props}) => {
             <div className="bg"></div>
             <div className=" content">
                 <div className="new-entry-content">
-                    <form>
-                        <h2>Tell about your day:</h2>
+                    <form className="new-entry-form">
+                        <h2 className="new-entry-form-title">Tell about your day:</h2>
                         <textarea maxLength="1000" className="new-entry-form-input" required/>
+                        <button type="submit" className="new-entry-form-btn">Submit</button>
                     </form>
                 </div>
             </div>
